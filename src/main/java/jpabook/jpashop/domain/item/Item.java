208044305d292db381fc6b484daf10df8a48057e
pkminsu <techrosseo@naver.com>
2025-04-03 17:24:16 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")//일대다 다대일로 풀어주는 중간관계
+    private List<Category> categories = new ArrayList<>();
+}
